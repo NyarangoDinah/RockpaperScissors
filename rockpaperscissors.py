9508627405af199cc402bbb1a8d6e7d59c700f07
NyarangoDinah <nyarangodinah@gmail.com>
2023-11-14 07:15:48 +0300

    Initial commit

---
+++ rockpaperscissors.py
@@ -0,0 +1,17 @@
+import random
+
+def get_choices():
+  player_choice = input("Enter a choice(rock, paper, scissors: ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return choices
+
+def check_win(player, computer):
+  print(f"You chose  {player}, computer chose {computer}")
+  if player == computer:
+    return "Its a tie!"
+
+
+check_win("rock", "paper")
+
